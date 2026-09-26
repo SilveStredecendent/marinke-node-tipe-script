528c71e819eb2dd5363b23e4e939e170f3f8053e
Herbert de Sousa Santos <117468625+SilveStredecendent@users.noreply.github.com>
2026-09-26 02:54:07 +0000

    feat: adicionar configuração do Jest e TypeScript para testes

---
+++ src/jest.config.js
@@ -0,0 +1,27 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.test.json' }],
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/app.ts',
+    '!src/config/**',
+    '!src/models/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
